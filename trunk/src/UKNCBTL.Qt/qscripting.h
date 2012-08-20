fbedc2d6f04b25652c05ef562c49a7be4ee98fe0
nzeemin <nzeemin@d259c8a2-7647-0410-94c4-d9ba3b64c92c>
2012-08-20 20:24:16 +0000

    [*] UKNCBTL.Qt scripting: emulator uptime, CPU/PPU registers.

--- trunk/src/UKNCBTL.Qt/qscripting.h
+++ trunk/src/UKNCBTL.Qt/qscripting.h
@@ -10,15 +10,53 @@
 
 
 class QScriptWindow;
+class CProcessor;
+
+
+class QEmulatorProcessor : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString name READ getName)
+    Q_PROPERTY(ushort sp READ getSP)
+    Q_PROPERTY(ushort pc READ getPC)
+    Q_PROPERTY(ushort psw READ getPSW)
+    Q_PROPERTY(bool halt READ isHalt)
+public:
+    QEmulatorProcessor(CProcessor* processor);
+
+public slots:
+    /**
+      Get the name of the processor: "CPU" or "PPU".
+      */
+    QString getName();
+    /**
+      Get register value.
+      */
+    ushort getReg(int regno);
+    ushort r(int regno) { return getReg(regno); }
+    ushort getSP() { return getReg(6); }
+    ushort getPC() { return getReg(7); }
+    ushort getPSW();
+    /**
+      Get HALT/USER mode flag.
+      */
+    bool isHalt();
+
+    //TODO: Memory access -- readWord(addr), readByte(addr)
+
+private:
+    CProcessor* m_processor;
+};
+
 
 class QEmulator : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(float uptime READ getUptime)
+    Q_PROPERTY(QObject* cpu READ getCPU)
+    Q_PROPERTY(QObject* ppu READ getPPU)
 public:
-    QEmulator(QScriptWindow * window) : m_window(window) { }
-
-public:
-
+    QEmulator(QScriptWindow * window);
 
 public slots:
     /**
@@ -29,6 +67,10 @@ public slots:
       Runs the emulator for the given number of frames. One second is 25 frames.
       */
     bool run(int frames);
+    /**
+      Get emulator uptime, in seconds.
+      */
+    float getUptime();
     /**
       Sets breakpoint address for the next run() call.
       */
@@ -57,15 +99,23 @@ public slots:
       Detach the floppy image file -- remove the disk.
       */
     void detachFloppy(int slot);
+    /**
+      Get CPU object.
+      */
+    QObject* getCPU() { return &m_cpu; }
+    /**
+      Get PPU object.
+      */
+    QObject* getPPU() { return &m_ppu; }
 
     //TODO: Keyboard keys press/release
-    //TODO: getUptime()
     //TODO: Attach/detach hard disk images
-    //TODO: CPU/PPU registers, memory
     //TODO: Change screen modes, sound on/off
 
 private:
     QScriptWindow * m_window;
+    QEmulatorProcessor m_cpu;
+    QEmulatorProcessor m_ppu;
 };
 
 
